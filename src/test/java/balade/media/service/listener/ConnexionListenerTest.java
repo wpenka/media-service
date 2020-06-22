b5c324eb18d52bf1ac6507f92cf2c492fcbbad0c
williams.m.penka <williams.m.penka@accenture.com>
2020-06-22 21:32:45 +0400

    Added mockito and assertj

--- src/test/java/balade/media/service/listener/ConnexionListenerTest.java
+++ src/test/java/balade/media/service/listener/ConnexionListenerTest.java
@@ -11,7 +11,6 @@ import org.junit.jupiter.api.Timeout;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.TestPropertySource;
-
 import java.net.URISyntaxException;
 import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.LinkedBlockingQueue;
